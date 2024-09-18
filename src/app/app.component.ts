6442d145f13538b4e8ade206e701fd6f62b587f0
Dat Trinh <dat.trinh@evizi.com>
2024-09-18 15:19:52 +0700

    folder change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,11 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
-import { VideoPlayerComponent } from "./features/teaser/video-player/video-player.component";
+import { VideoPlayerComponent } from "./pages/homepage/teaser/video-player/video-player.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
 import { HeaderComponent } from './shared/components/header/header.component';
-import { ProductListComponent } from "./features/product-list/product-list.component";
-import { TeaserComponent } from "./features/teaser/teaser.component";
+import { ProductListComponent } from "./pages/homepage/product-list/product-list.component";
+import { TeaserComponent } from "./pages/homepage/teaser/teaser.component";
   @Component({
     selector: 'app-root',
     standalone: true,
