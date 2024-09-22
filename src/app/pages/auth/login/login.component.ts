f0d7d19b04412c6c8ac7bb02c32de15acbb482bd
Mai Do Thi Ngoc <mai.do@evizi.com>
2024-09-23 01:17:06 +0700

    add call api login, register

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { RegisterComponent } from '../register/register.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RegisterComponent],
+  imports: [RegisterComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
+@Injectable()
 export class LoginComponent {
-  constructor(private router: Router) {}
-
+  constructor(private router: Router, private http: HttpClient) {}
+  private baseApi = 'http://localhost:5243/api/';
+  formLogin = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl(),
+  });
+  loginUser() {
+    let formValue = this.formLogin.value;
+    console.log(formValue)
+    let response = this.http.post(this.baseApi + 'Users/login', formValue, {
+      responseType: 'json',
+    });
+    response.subscribe(
+      (response: any) => {
+        console.log('Response:', response); // Log the actual response
+        this.router.navigate(['/']);
+      },
+      (error) => {
+        console.error('Error:', error); // Handle and log any error
+      }
+    );
+  }
   goToRegister() {
     this.router.navigate(['/register']);
   }
--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, Injectable } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Console, log } from 'console';
+import { response } from 'express';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
+@Injectable()
 export class RegisterComponent {
-
+  private baseApi = 'http://localhost:5243/api/';
+  constructor(private http: HttpClient, private router: Router) {}
+  formRegister = new FormGroup({
+    username: new FormControl(),
+    email: new FormControl(),
+    password: new FormControl(),
+    confirm_password: new FormControl(),
+  });
+  registerUser() {
+    let formValue = this.formRegister.value;
+    console.log(formValue);
+    if (
+      formValue.password != null &&
+      formValue.password == formValue.confirm_password
+    ) {
+      if (formValue.username != null && formValue.email != null) {
+        let response = this.http.post(
+          this.baseApi + 'Users/register',
+          formValue,
+          { responseType: 'text' }
+        );
+        response.subscribe(
+          (response: any) => {
+            console.log('Response:', response); // Log the actual response
+            this.router.navigate(['/login']);
+          },
+          (error) => {
+            console.error('Error:', error); // Handle and log any error
+          }
+        );
+      } else {
+        console.warn('Username is null or email is null');
+      }
+    } else {
+      console.warn('Password is null or confirm password not match');
+    }
+  }
 }
