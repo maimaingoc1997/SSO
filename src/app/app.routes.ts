bccb7576e681f5247b390b1c05af440ca4f5f406
Dat Trinh <dat.trinh@evizi.com>
2024-09-24 15:57:32 +0700

    Revert "Merge homepage"
    This reverts commit 43e55bbaacf5ce44f70652c57c7750e800ad8189.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,3 @@
 import { Routes } from '@angular/router';
-import { CartComponent } from './pages/homepage/cart/cart/cart.component';
-import { LoginComponent } from './pages/homepage/login/login/login.component';
-import { ProductListComponent } from './pages/homepage/product-list/product-list.component';
-import { TeaserComponent } from './pages/homepage/teaser/teaser.component';
-import { WishlistComponent } from './pages/homepage/wishlist/wishlist.component';
-export const routes: Routes = [
 
-    {
-        path: '',
-        children: [
-            { path: '', component: TeaserComponent, outlet: 'teaser' },
-            { path: '', component: ProductListComponent, outlet: 'product-list' }
-        ]
-    },
-    { path: 'login', component: LoginComponent },
-    { path: 'cart', component: CartComponent },
-    { path: 'wishlist', component: WishlistComponent },
-    {
-        path: ':category',
-        children: [
-            { path: '', component: TeaserComponent, outlet: 'teaser' },
-            { path: '', component: ProductListComponent, outlet: 'product-list' }
-        ]
-    }
-];
+export const routes: Routes = [];
