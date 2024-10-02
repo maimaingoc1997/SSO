2b5b2df2c4d24a988e4ced63c6b88d2548ca3154
Dat Trinh <dat.trinh@evizi.com>
2024-10-02 11:44:17 +0700

    product-details

--- src/app/pages/product-details/product-detail/product-detail.component.ts
+++ src/app/pages/product-details/product-detail/product-detail.component.ts
@@ -6,10 +6,13 @@ import { CartService } from '../../../services/cart.service';
 import { AuthService } from '../../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { Cart } from '../../../shared/models/cart/cart.model';
+import { MatDialogModule } from '@angular/material/dialog';
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+            MatDialogModule
+  ],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss'
 })
@@ -17,6 +20,7 @@ export class ProductDetailComponent implements OnInit {
 
   selectedProduct: Product | null = null;
   productId: string | null = null;
+  showNotification: boolean = false;
   constructor(private productService: ProductService,
     private route: ActivatedRoute,
     private cartService: CartService,
@@ -42,26 +46,30 @@ export class ProductDetailComponent implements OnInit {
 
   addToCart(item: any) {
     const userId = this.authService.getUserId();
-    if(this.selectedProduct){
+    if (this.selectedProduct) {
       const cartItem: Cart = {
-      cartId: 0, 
-      productId: this.selectedProduct.id, 
-      productName: this.selectedProduct.name, 
-      productPrice: this.selectedProduct.price, 
-      image: this.selectedProduct.image || '', 
-      size: this.selectedProduct.sizeId, 
-      quantity: 1, 
-      isWishlist: 0 
-    };
-    this.cartService.addToCart(cartItem, userId).subscribe({
-      next: (response) => {
-        this.cartService.updateCartItemCount(item.length);
-        console.log('Item added to cart', response);
-      },
-      error: (err) => {
-        console.error("Failed to add item to cart", err)
-      }
-    });
+        cartId: 0,
+        productId: this.selectedProduct.id,
+        productName: this.selectedProduct.name,
+        productPrice: this.selectedProduct.price,
+        image: this.selectedProduct.image || '',
+        size: this.selectedProduct.sizeId,
+        quantity: 1,
+        isWishlist: 0
+      };
+
+      this.cartService.addToCart(cartItem, userId).subscribe({
+        next: () => {
+          this.showNotification = true; // Show the notification
+          // Automatically hide notification after 3 seconds
+          setTimeout(() => {
+            this.showNotification = false;
+          }, 1000);
+        },
+        error: (err) => {
+          console.error("Failed to add item to cart", err);
+        }
+      });
     }
   }
 }
